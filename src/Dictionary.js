b1f59c5fab3010f5202079706ac331f0f4080cc7
Azar Hosseininezhad <azar.hn9@gmail.com>
2021-11-15 16:36:14 +0330

    React Dictionary Project / Meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Results from "./Results"
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-  let [results, setResults]= useState({});
-function handleResponse(response)
-{
-    console.log(response.data[0]);
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
     setResults(response.data[0]);
-}
+  }
+
   function search(event) {
     event.preventDefault();
 
-    // Documentation: https://dictionaryapi.dev/
-    let apiUrl="https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}";
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
