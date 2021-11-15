04d3a4c1f74807ffb3aa0fd2a195692c69280218
Azar Hosseininezhad <azar.hn9@gmail.com>
2021-11-15 14:35:17 +0330

    add API documentation url

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -10,6 +10,8 @@ function handleResponse(response)
 }
   function search(event) {
     event.preventDefault();
+
+    // Documentation: https://dictionaryapi.dev/
     let apiUrl="https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}";
     axios.get(apiUrl).then(handleResponse);
   }
